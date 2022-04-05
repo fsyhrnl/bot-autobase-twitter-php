d1adfd454941eba9c989977fa240840f6d774f9d
Ferry Syahrinal <30984368+ferrysyahrinal@users.noreply.github.com>
2022-04-06 04:43:05 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+require 'autoload.php';
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_TOKEN', ''); 
+define('OAUTH_TOKEN_SECRET', ''); 
+//define('OAUTH_CALLBACK', ''); //callback url, isi jika dibutuhkan. (optional)
+
+define('WEBHOOK_URL', ''); //https://domain-kamu.com/twitter/webhook.php
+define('ENV_LABEL', ''); //Dev environment label
+
+define('TRIGGER_WORD', '[dog]');
+define('ADMIN_ID', ['965702083', '', '']); //User ID Admin (optional)
+define('MINIMAL_FOLLOWERS', '10');
+define('SLEEP_TIME', '20'); //delay ketika post tweet (detik)
+
+if (!CONSUMER_KEY || !CONSUMER_SECRET || !WEBHOOK_URL || !ENV_LABEL) {
+    echo 'CONSUMER_KEY, CONSUMER_SECRET, WEBHOOK_URL, and ENV_LABEL variables must be set';
+}
---
+++ setWebhook.php
@@ -0,0 +1,17 @@
+<?php
+require 'config.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
+
+$content = $connection->post('account_activity/all/'.ENV_LABEL.'/webhooks', ['url' => WEBHOOK_URL]);
+print_r($content);
+echo "\n\n";
+$content2 = $connection->post("account_activity/all/".ENV_LABEL."/subscriptions");
+print_r($content2);
+echo "\n\n";
+$request_token = $connection->oauth2('oauth2/token', ['grant_type' => 'client_credentials']);
+$newConnection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token->access_token);
+$content3 = $newConnection->get("account_activity/all/".ENV_LABEL."/subscriptions/list");
+print_r($content3);
+
