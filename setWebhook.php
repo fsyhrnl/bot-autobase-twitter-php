f2395e32a46ace490740f90c4edb694533951cb5
Ferry Syahrinal <30984368+ferrysyahrinal@users.noreply.github.com>
2022-04-06 17:36:06 +0700

    add <br>
    newline if opened via browser

--- setWebhook.php
+++ setWebhook.php
@@ -7,9 +7,11 @@ $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH
 $content = $connection->post('account_activity/all/'.ENV_LABEL.'/webhooks', ['url' => WEBHOOK_URL]);
 print_r($content);
 echo "\n\n";
-$content2 = $connection->post("account_activity/all/".ENV_LABEL."/subscriptions");
+echo "<br>";
+$content2 = $connection->post('account_activity/all/'.ENV_LABEL.'/subscriptions');
 print_r($content2);
 echo "\n\n";
+echo "<br>";
 $request_token = $connection->oauth2('oauth2/token', ['grant_type' => 'client_credentials']);
 $newConnection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token->access_token);
 $content3 = $newConnection->get("account_activity/all/".ENV_LABEL."/subscriptions/list");
